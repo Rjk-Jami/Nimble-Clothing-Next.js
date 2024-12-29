6c1d0c3e8f0beaf4791643b899d94c9e5c31675d
Rjk-Jami <rjkhan.1221@gmail.com>
2024-12-30 02:59:17 +0600

    product card

--- src/app/(shopMother)/shop/page.jsx
+++ src/app/(shopMother)/shop/page.jsx
@@ -10,14 +10,17 @@ import ProductsCard from "@/components/ProductsCard/ProductsCard";
 const page = () => {
   const dispatch = useDispatch();
   const { products, isLoading, isError, error } = UseGetProductsWithFilter();
-
+  const { 2: colTwo, 3: colThree, 4: colFour } = useSelector(
+    (state) => state.filter.displayTag.filterDisplaysControl
+  );
+  console.log(colTwo, colThree, colFour, "state");
   return (
     <div>
       <div>
         {/*products loading  */}
         {isLoading && <Loading />}
       </div>
-      <div className=" grid grid-cols-1 md:grid-cols-4 gap-2">
+      <div className={`grid grid-cols-1 ${colTwo ? ' md:grid-cols-2' :''} ${colThree ? ' md:grid-cols-3' :''} ${colFour  ? ' md:grid-cols-4' :''} gap-5`}>
         {products?.map((product, i) => (
           <ProductsCard key={i} product={product}></ProductsCard>
         ))}
--- src/components/ProductsCard/ProductsCard.jsx
+++ src/components/ProductsCard/ProductsCard.jsx
@@ -2,25 +2,44 @@ import Image from "next/image";
 import React from "react";
 import { FaBangladeshiTakaSign } from "react-icons/fa6";
 import { FaCartPlus } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { FaHeart } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa";
+import { GrCompare } from "react-icons/gr";
 
 const ProductsCard = ({ product }) => {
+  const {
+    2: colTwo,
+    3: colThree,
+    4: colFour,
+  } = useSelector((state) => state.filter.displayTag.filterDisplaysControl);
   return (
-    <div className="flex flex-col group">
-      <div className=" relative">
-      <span className="absolute inset-x-0 bottom-0 transition-all delay-75 w-full  bg-black dark:bg-white opacity-0 group-hover:opacity-100 flex justify-center items-center">
-
-<FaCartPlus className="text-white dark:text-black text-3xl m-2"></FaCartPlus>
-      </span>
-
+    <div className="flex flex-col group border border-gray-200 dark:border-gray-700 pb-2  shadow-md cursor-pointer ">
+      <div className=" relative overflow-hidden">
+        <span className="absolute inset-x-0 bottom-0 transform translate-y-5 bg-black dark:bg-white group-hover:translate-y-0 transition-all duration-300 ease-out delay-100 opacity-0 group-hover:opacity-100 flex justify-center items-center ">
+          <FaCartPlus
+            className={`text-white dark:text-black ${
+              colTwo ? "text-3xl" : "text-2xl"
+            } m-2`}
+          ></FaCartPlus>
+        </span>
+        <div className="absolute top-0 right-0 transform translate-y-5 group transition-all bg-black dark:bg-white duration-300 ease-out delay-100 group-hover:translate-x-[-20px] opacity-0 group-hover:opacity-100 flex flex-col gap-3 items-center p-2">
+          <div className="tooltip  tooltip-left " data-tip="Compare">
+            <GrCompare className="text-white dark:text-black text-2xl"></GrCompare>
+          </div>
+          <div className="tooltip  tooltip-left " data-tip="Add to wishlist">
+            <FaRegHeart className="text-white dark:text-black text-2xl "></FaRegHeart>
+          </div>
+        </div>
         <Image
+          className="cursor-default"
           src={product?.image}
           alt={product.name}
           width={430}
           height={430}
         />
-        
       </div>
-      <div className="flex flex-col gap-1  justify-center items-center">
+      <div className="z-50 flex flex-col gap-1  justify-center items-center">
         <h1 className="text-sm font-bold">{product.name}</h1>
         <p className="text-sm font-semibold ">{product.categories}</p>
         <div className=" flex gap-1 items-center justify-center">
@@ -30,8 +49,12 @@ const ProductsCard = ({ product }) => {
           <p className="text-sm font-semibold text-orange-500">
             {product.current_price} <span></span>
           </p>
-          <p><FaBangladeshiTakaSign className="text-xs 
-          "></FaBangladeshiTakaSign></p>
+          <p>
+            <FaBangladeshiTakaSign
+              className="text-xs 
+          "
+            ></FaBangladeshiTakaSign>
+          </p>
         </div>
       </div>
     </div>
