b3a401713863cd99ae48705133651e5844eb0e47
Rjk-Jami <rjkhan.1221@gmail.com>
2025-01-19 14:55:35 +0600

    env setpup

--- redux/api/rootApi.js
+++ redux/api/rootApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userLogOut } from "../auth/authSlice";
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://nimblenext-backend.vercel.app/api/v1",
+  baseUrl: process.env.NEXT_PUBLIC_SERVER_API,
   credentials:"include",
   prepareHeaders: (headers) => {
     headers.set("Content-Type", "application/json");
