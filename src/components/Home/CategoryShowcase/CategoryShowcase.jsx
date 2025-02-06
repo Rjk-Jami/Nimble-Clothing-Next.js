95a365711384ea77d59515b190e4d5536eb25afc
Rjk-Jami <rjkhan.1221@gmail.com>
2025-02-07 02:23:28 +0600

    add products showcage

--- src/components/Home/CategoryShowcase/CategoryShowcase.jsx
+++ src/components/Home/CategoryShowcase/CategoryShowcase.jsx
@@ -1,10 +1,29 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useGetProductsMutation } from "../../../../redux/products/productsApi";
+import Image from "next/image";
+import boxers from "../../../../public/boxer.png";
+import hoodies from "../../../../public/Hoodie.png";
+import pants from "../../../../public/Joggers.png";
+import sweatshirt from "../../../../public/Sweatshirt.png";
+import tShirts from "../../../../public/DropShoulderT-shirt.png";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import Link from "next/link";
+import { navProductsItemsData } from "@/components/features/produtsMenu";
 
 const CategoryShowcase = () => {
   const [getProducts, { isLoading, isError, error }] = useGetProductsMutation();
   const [products, setProducts] = useState([]);
+  const categories = [
+    { tagName: "Boxers", image: boxers },
+    { tagName: "Hoodies", image: hoodies },
+    { tagName: "Pants", image: pants },
+    { tagName: "Sweatshirt", image: sweatshirt },
+    { tagName: "T-Shirts", image: tShirts },
+  ];
   useEffect(() => {
     const fetchProducts = async () => {
       try {
@@ -12,7 +31,7 @@ const CategoryShowcase = () => {
         const allProducts = result?.data?.allProduct || [];
 
         if (allProducts.length > 0) {
-          setProducts(allProducts); 
+          setProducts(allProducts);
         }
       } catch (err) {
         console.error("Error fetching products:", err);
@@ -21,8 +40,65 @@ const CategoryShowcase = () => {
     fetchProducts();
   }, [getProducts]);
   console.log(products);
-  
-  return <div></div>;
+
+  return (
+    <div className="mt-10">
+      <div className="">
+        <Swiper
+          slidesPerView={2}
+          spaceBetween={0}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 0,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 0,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 0,
+            },
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          {categories.map((category, index) => (
+            <SwiperSlide key={index}>
+              {/* find Link */}
+              <Link href={navProductsItemsData.find(nav=>nav.name === category.tagName).url}><div className=" relative overflow-hidden group">
+                <span className="absolute z-40 inset-x-0 bottom-0 transform translate-y-5 bg-white dark:bg-black group-hover:translate-y-0 transition-all duration-300 ease-out delay-100 opacity-0 group-hover:opacity-100 flex justify-start items-center">
+                  <div className="flex flex-col px-4 py-2">
+                    <p className="text-3xl font-bold  ">{category?.tagName}</p>
+                    <p>
+                      {
+                        products?.filter(
+                          (p) => p.categories === category.tagName
+                        ).length
+                      }
+                    </p>
+                  </div>
+                </span>
+                <div className="overflow-hidden lg:h-[450px] md:h-[300px] h-[300px] z-0">
+                  <Image
+                    className="mx-auto o cursor-default transform hover:scale-125 transition-transform duration-300 ease-out"
+                    src={category?.image}
+                    alt={category?.tagName}
+                    // width={330}
+                    width={330}
+                  ></Image>
+                </div>
+              </div></Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryShowcase;
