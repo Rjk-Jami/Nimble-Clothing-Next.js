40cf00131549999b3ba11658cb2a75896e8ea5f1
Rjk-Jami <rjkhan.1221@gmail.com>
2025-02-06 15:11:34 +0600

    fix Privacy Policy

--- src/app/PrivacyPolicy/layout.jsx
+++ src/app/PrivacyPolicy/layout.jsx
@@ -1,9 +1,11 @@
 import Footer from '@/components/Footer'
+import Header from '@/utils/Header/Header'
 import React from 'react'
 
 const layout = ({children}) => {
   return (
     <div>
+        <Header>Privacy Policy</Header>
       {children}
       <Footer></Footer>
     </div>
