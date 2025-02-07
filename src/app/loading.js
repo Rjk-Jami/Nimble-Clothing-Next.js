b53c673f5f3ed8f00c86ed8d66117a40f863a762
Rjk-Jami <rjkhan.1221@gmail.com>
2025-02-08 01:51:19 +0600

    add a loader

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,13 +1,33 @@
-const Loading = () => {
-    return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 dark:bg-white/50">
-  <div className="flex flex-col items-center">
-    <div className="w-24 h-24 border-8 border-gray-200 border-t-8 border-t-black rounded-full animate-spin"></div>
-    <p className="mt-4 text-gray-600">Loading</p>
-  </div>
-</div>
+"use client";
+import { useEffect, useState } from "react";
+import { ColorRing, RotatingSquare } from "react-loader-spinner";
 
-    );
+const Loading = () => {
+  const [color, setColor] = useState("#000"); // Default to black
+
+  useEffect(() => {
+    // Check if dark mode is enabled
+    const isDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    setColor(isDarkMode ? "#fff" : "#00000"); // White for dark mode, Black for light mode
+  }, []);
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 dark:bg-black/30">
+      <div className="flex flex-col items-center">
+        <ColorRing
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="color-ring-loading"
+          wrapperStyle={{}}
+          wrapperClass="color-ring-wrapper"
+          colors={["#abbd81", "#f47e60", "#f8b26a", "#e15b64", "#849b87"]}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
