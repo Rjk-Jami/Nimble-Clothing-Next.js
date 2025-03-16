424ecf3b6540a76c3454d4f6a67f32f0aaddce88
Jami Khan <77094939+Rjk-Jami@users.noreply.github.com>
2025-03-16 23:02:32 +0600

    Update authSlice.js

--- redux/auth/authSlice.js
+++ redux/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit"
 const initialState = {
     user: "",
     token: "",
-    errorMassage: ''
+    errorMassage: ""
 }
 
 export const authSlice = createSlice({
